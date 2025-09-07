a13199fc1141fd165fd51d55a5f13ed13a0387f3
jazgamarra <j444min@gmail.com>
2025-09-07 13:30:43 -0300

    clase 1

--- src/app/app.ts
+++ src/app/app.ts
@@ -7,6 +7,23 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
+
 export class App {
-  protected readonly title = signal('project01');
+  nombre = 'Rodriguez Pablo';
+  edad = 40;
+  email = 'rpablo@gmail.com';
+  sueldos = [1700, 1600, 1900];
+  activo = true;
+  esActivo() {
+    if (this.activo)
+      return 'Trabajador Activo';
+    else
+      return 'Trabajador Inactivo';
+  }
+ ultimos3Sueldos() {
+    let suma = 0;
+    for (let x = 0; x < this.sueldos.length; x++)
+      suma += this.sueldos[x];
+    return suma;
+  }
 }
