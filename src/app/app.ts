51bce4e3a04b2c9eaf3aff71f26bac6702a081d4
jazgamarra <j444min@gmail.com>
2025-09-07 18:52:24 -0300

    clase 7 - formulario de nombre y apellido

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,23 +1,14 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Selectornumerico } from './selectornumerico/selectornumerico';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Selectornumerico],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  @ViewChild('selector1') selector1!: Selectornumerico;
-  @ViewChild('selector2') selector2!: Selectornumerico;
-
-  fijarSelector1(valor:number) {
-    this.selector1.fijar(valor);
-  }
-
-  fijarSelector2(valor:number) {
-    this.selector2.fijar(valor);
-  }
+  nombre = '';
+  apellido = '';
 }
-  
