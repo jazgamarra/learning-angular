b34dc31c9a86c9b467fc3ddb324655f983fea7fe
jazgamarra <j444min@gmail.com>
2025-09-07 13:48:52 -0300

    clase 2

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,4 +1,4 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
@@ -7,23 +7,26 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-
 export class App {
   nombre = 'Rodriguez Pablo';
+
   edad = 40;
-  email = 'rpablo@gmail.com';
-  sueldos = [1700, 1600, 1900];
-  activo = true;
-  esActivo() {
-    if (this.activo)
-      return 'Trabajador Activo';
-    else
-      return 'Trabajador Inactivo';
-  }
- ultimos3Sueldos() {
-    let suma = 0;
-    for (let x = 0; x < this.sueldos.length; x++)
-      suma += this.sueldos[x];
-    return suma;
+
+  articulos = [{
+    codigo: 1,
+ descripcion: 'naranjas',
+    precio: 540
+  }, {
+    codigo: 2,
+    descripcion: 'manzanas',
+    precio: 900
+  }, {
+    codigo: 3,
+    descripcion: 'peras',
+    precio: 490
+  }];
+
+  generarNumero() {
+    return Math.floor(Math.random() * 3) + 1;
   }
 }
